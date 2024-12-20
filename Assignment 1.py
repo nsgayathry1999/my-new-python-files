46803a5f4d1fd0e6cd64b0d0aa0b872ed3c8c575
nsgayathry1999 <gayathryns18@gmail.com>
2024-12-20 18:45:05 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,74 @@
+#exercise 1
+#print("gayathry")
+#print(15962)
+#print("gayathryns199@gamil.com")
+
+
+#excercise 2
+#name=input("enter your name:")
+#student_number=input("enter your student number:")
+#email=input("enter your email:")
+#print(f"{name}\n{student_number}\n{email}")
+
+#excercise 3
+#num_1=14
+#num_2=7
+#print("sum=",num_1+num_2,"substraction=",num_1-num_2,"multiplication=",num_1*num_2,"division=",num_1/num_2)
+
+#num_1=14
+#num_2=7
+#print(f"{num_1}+{num_2}={num_1+num_2}")
+#print(f"{num_1}-{num_2}={num_1-num_2}")
+#print(f"{num_1}*{num_2}={num_1*num_2}")
+#print(f"{num_1}/{num_2}={num_1/num_2}")
+
+#Exercise 4 Write Python code that displays the numbers from 1 to 5 as steps.
+#for i in range(1,6):
+ # print("step",i)
+
+
+#Exercise 5 Write Python code that outputs the following sentence (including the quotation marks and
+# line break) to the screen:
+#print('"SDK" stands for"Software Development Kit",\nwhereas\n"IDE"stands for "integrated development Environment".')
+
+#Exercise 6 Practice and check the output print("python is an \"awesome\" language.") print("python\n\t2023") print('I\'m from Entri.\b') print("\65") print("\x65") print("Entri", "2023", sep="\n") print("Entri", "2023", sep="\b") print("Entri", "2023", sep="*", end="\b\b\b\b")
+#print("python is an \"awesome\" language.")
+#print("python\n\t2023")
+#print('I\'m from Entri.\b')
+#print("\65")
+#print("\x65")
+#print("Entri", "2023", sep="\n")
+#print("Entri", "2023", sep="\b")
+#print("Entri", "2023", sep="*", end="\b\b\b\b")
+
+#Exercise 7 Define the variables below. Print the types of each variable. What is the sum of your variables? (Hint: use a type conversion function.) What datatype is the sum? num=23 textnum="57" decimal=98
+#num=23
+#text_num="57"
+#decimal=98.3
+#print(type(num))
+#print(type(text_num))
+#print(type(decimal))
+
+#total_sum=num+int(text_num)+decimal
+#print(f"the sum is:{total_sum}")
+#print(type(total_sum))
+
+#Exercise 8
+#days_in_year=365
+#hours_in_day=24
+#minutes_in_hour=60
+#total_minutes=days_in_year*hours_in_day*minutes_in_hour
+#print(f"There are{total_minutes}minutes in a year.")
+
+#Exercise 9
+name=input("please enter your name:")
+print(f"Hi{name},Welcome to python programming:)")
+
+
+
+
+
+
+
+
+
