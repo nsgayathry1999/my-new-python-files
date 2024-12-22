0cce09048b2d97ebd4f2943b136278f5ad83cdbf
nsgayathry1999 <gayathryns18@gmail.com>
2024-12-22 18:39:34 +0530

    Add files via upload
    Here file shows the data structures in python that list, tuple, set and dictionary

---
+++ Assignment 2.py
@@ -0,0 +1,38 @@
+# excersise 1
+# Create a list of 5 random numbers and print the list
+#rand_num=[20,40,30,40,50]
+#print(rand_num)
+# Insert 3 new values to the list and print the updated list.
+# rand_num.extend([60,70,80])
+# print(rand_num)
+#Q3. Try to use a for loop to print each element in the list
+#for i in rand_num:
+#print(i)
+
+#Dictionary Exercise
+#Q1. Create a dictionary with keys 'name', 'age', and 'address' and values 'John', 25, and 'New York' respectively.
+person={"name":"John","age":25,"address":"New York"}
+print(person)
+#Q2. Add a new key-value pair to the dictionary created in Q1 with key 'phone' and value '1234567890'.
+person["phone"]="1234567890"
+print(person)
+
+#Topic: Set Exercise
+#Q1.Create a set with values 1, 2, 3, 4, and 5.
+#set={1,2,3,4,5}
+#print(set)
+#Q2. Add the value 6 to the set created in Q1.
+#set.add(6)
+#print(set)
+#Q3. Remove the value 3 from the set created in Q1.
+#set.remove(3)
+#print(set)
+
+#Topic:Tuple Exercise
+#Q1. Create a tuple with values 1, 2, 3, and 4
+# Q2. Print the length of the tuple created in Q1.
+# t1=(1,2,3,4)
+# print(t1)
+# print(len(t1))
+
+
